4492e87119c2fa202f4c2acd5c2c76a5f570e586
Nitin Singh <nitin.pro.004@gmail.com>
2025-12-11 12:11:47 +0530

    1

---
+++ q1.cpp
@@ -0,0 +1,30 @@
+// 921. Minimum Add to Make Parentheses Valid
+// LeetCode
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) 
+    {
+        stack<char>st;
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push('(');
+            }
+            else
+            {
+                if(!st.empty() && st.top()=='(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(')');
+                }
+            }
+        }
+        return st.size();
+    }
+};
